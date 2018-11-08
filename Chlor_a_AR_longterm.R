3e9af992a1b3b71152308ee01fe8512f093593ba
kellyloria <kelo2727@colorado.edu>
2018-11-08 12:10:13 -0700

    Done making figures for now

--- Chlor_a_AR_longterm.R
+++ Chlor_a_AR_longterm.R
@@ -9,6 +9,8 @@ library(ggplot2)
 library(scales)
 library(dplyr)
 library(tidyverse)
+library(lme4)
+library(lmerTest)
 
 
 # load in extended summer PCA values 
@@ -69,30 +71,45 @@ in_out_wc <- rbind(WQdat_in_lake, WQdat_out_lake)
 # function to calculate standard error around the mean 
 se <- function(x) {sd(x,na.rm=TRUE)/sqrt(length(x))}
 
-## average chlor-a for all inlake depths by year 
-WQdat_lake_ave <- aggregate(chl_a ~ year, data=WQdat_lake, FUN=mean) 
-WQdat_lake_ave$SE <- aggregate(chl_a ~ year, data=WQdat_lake, FUN=se)[,2]
+## annual average all chlor-a data aty GL4
+WQdat_all_ave <- aggregate(chl_a ~ year, data=WQdat1, FUN=mean) 
+WQdat_all_ave$SE <- aggregate(chl_a ~ year, data=WQdat1, FUN=se)[,2]
+
 
 # average annual chla at inlet
 WQdat_IN_ave <- aggregate(chl_a ~ year, data= WQdat_in_lake, FUN=mean) 
 WQdat_IN_ave$SE <- aggregate(chl_a ~ year, data=WQdat_in_lake, FUN=se)[,2]
 
+WQdat_IN_ave$depth <- "IN"
+
 # average annual chla at outlet
 WQdat_out_ave <- aggregate(chl_a ~ year, data= WQdat_out_lake, FUN=mean) 
 WQdat_out_ave$SE <- aggregate(chl_a ~ year, data=WQdat_out_lake, FUN=se)[,2]
+
+WQdat_out_ave$depth <- "OUT"
 
 # average annual chla at lake surface
 WQdat_0m_ave <- aggregate(chl_a ~ year, data= WQdat_lake_0m, FUN=mean) 
 WQdat_0m_ave$SE <- aggregate(chl_a ~ year, data=WQdat_lake_0m, FUN=se)[,2]
 
+WQdat_0m_ave$depth <- "0m"
+
 # average annual chla at metalimnion
 WQdat_3m_ave <- aggregate(chl_a ~ year, data= WQdat_lake_3m, FUN=mean) 
 WQdat_3m_ave$SE <- aggregate(chl_a ~ year, data=WQdat_lake_3m, FUN=se)[,2]
+
+WQdat_3m_ave$depth <- "3m"
 
 # average annual chla at hypolimnion 
 WQdat_9m_ave <- aggregate(chl_a ~ year, data= WQdat_lake_9m, FUN=mean) 
 WQdat_9m_ave$SE <- aggregate(chl_a ~ year, data=WQdat_lake_9m, FUN=se)[,2]
 
+WQdat_9m_ave$depth <- "9m"
+
+
+chlor_ave_all <- rbind(WQdat_IN_ave, WQdat_out_ave, WQdat_0m_ave, WQdat_3m_ave, WQdat_9m_ave)
+chlor_ave_inlake <- rbind(WQdat_0m_ave, WQdat_3m_ave, WQdat_9m_ave)
+in_out_ave <- rbind(WQdat_IN_ave, WQdat_out_ave)
 
 ### Combine extended summer PCA with subsetted data ###
 
@@ -100,13 +117,6 @@ WQdat_9m_ave$SE <- aggregate(chl_a ~ year, data=WQdat_lake_9m, FUN=se)[,2]
 comb_dat_WQ <- left_join(WQdat1, dt_summer[c("eco_year", "sumallPC1")],
                          by = c("year" = "eco_year"))
 
-## chlora for all in lake data and PCA
-comb_dat_lake_WQ <- left_join(WQdat_lake, dt_summer[c("eco_year", "sumallPC1")],
-                              by = c("year" = "eco_year"))
-
-## Annual Average chlora for all inlake data and PCA
-comb_dat_L_WQ <- left_join(WQdat_lake_ave, dt_summer[c("eco_year", "sumallPC1")],
-                           by = c("year" = "eco_year"))
 
 ## chlora for all inlet lake data and PCA
 comb_dat_in_WQ <- left_join(WQdat_in_lake, dt_summer[c("eco_year", "sumallPC1")],
@@ -116,9 +126,39 @@ comb_dat_in_WQ <- left_join(WQdat_in_lake, dt_summer[c("eco_year", "sumallPC1")]
 comb_dat_in_out_WQ <- left_join(in_out_wc, dt_summer[c("eco_year", "sumallPC1")],
                                 by = c("year" = "eco_year"))
 
+
+
+
+############################################
+
+## Annual Average all chlora at GL4 and  PCA
+comb_dat_all_WQ <- left_join(WQdat_all_ave, dt_summer[c("eco_year", "sumallPC1")],
+                            by = c("year" = "eco_year"))
+
+## Annual all inlake chlora  with depth at GL4 and  PCA
+comb_dat_inlake_WQ <- left_join(chlor_ave_inlake, dt_summer[c("eco_year", "sumallPC1")],
+                             by = c("year" = "eco_year"))
+
 ## Annual Average chlora for hypo and PCA
 comb_dat_9m_WQ <- left_join(WQdat_9m_ave, dt_summer[c("eco_year", "sumallPC1")],
                             by = c("year" = "eco_year"))
+
+## Annual Average chlora for meta and PCA
+comb_dat_3m_WQ <- left_join(WQdat_3m_ave, dt_summer[c("eco_year", "sumallPC1")],
+                            by = c("year" = "eco_year"))
+
+## Annual Average chlora for surface and PCA
+comb_dat_0m_WQ <- left_join(WQdat_0m_ave, dt_summer[c("eco_year", "sumallPC1")],
+                            by = c("year" = "eco_year"))
+
+## Annual Average chlora for IN and OUT and PCA
+comb_dat_INOUT_WQ <- left_join(in_out_ave, dt_summer[c("eco_year", "sumallPC1")],
+                            by = c("year" = "eco_year"))
+
+## chlora for all average in lake data and PCA
+comb_dat_lake_WQ <- left_join(chlor_ave_all, dt_summer[c("eco_year", "sumallPC1")],
+                              by = c("year" = "eco_year"))
+
 
 #########################################################################
 ### Visualize relationships of Chlor overtime by observation location ###
@@ -248,65 +288,270 @@ legend("topleft", legend=c("Inlet", "Outlet", "0m", "3m", "9m"),
 
 #dev.off()
 
+#png("Chlor_A overtime(IN and OUT).png",  
+#    width = 5.25,
+#    height = 3.25,
+#    units = "in",
+#    res = 1200,
+#    pointsize = 4 )
+
+## Average annual chla for all lake locations
+
+plot(NA, NA, xlim = c(2000, 2017), ylim = c(0, 10), xlab = "Year",
+     ylab = "Average annual chlor-A in GL4", cex.lab=1.5, cex.axis=1.2, 
+     xaxt="n")
+axis(1, at=c(2000:2017), cex.axis=1.5)
+
+lines(y = WQdat_IN_ave$chl_a, x= WQdat_IN_ave$year, lty =1, lwd = 1, col= "lightseagreen")
+lines(y = WQdat_out_ave$chl_a,  x= WQdat_out_ave$year, lty =1, lwd = 1, col= "cadetblue3")
+
+points(y = WQdat_IN_ave$chl_a, x= WQdat_IN_ave$year, col= "lightseagreen", pch=19) 
+arrows(WQdat_IN_ave$year, (WQdat_IN_ave$chl_a + WQdat_IN_ave$SE), 
+       WQdat_IN_ave$year, (WQdat_IN_ave$chl_a - WQdat_IN_ave$SE), 
+       length=0.0, code=3, lwd=0.5, col= "lightseagreen")
+
+
+points(y = WQdat_out_ave$chl_a, x= WQdat_out_ave$year, col= "cadetblue3", pch=19) 
+arrows(WQdat_out_ave$year, (WQdat_out_ave$chl_a + WQdat_out_ave$SE), 
+       WQdat_out_ave$year, (WQdat_out_ave$chl_a - WQdat_out_ave$SE), 
+       length=0.0, code=3, lwd=0.5, col= "cadetblue3")
+
+
+legend("topleft", legend=c("Inlet", "Outlet", "0m", "3m", "9m"),
+       col=c("lightseagreen", "cadetblue3", "darkolivegreen3", "darkolivegreen4", "darkgreen" ), lty=1, cex=1.2)
+
+#dev.off()
+
+
+
 
 # plot chlor-a for in lake averages and pca for extened summer add in linear model 
-qplot(sumallPC1, chl_a, data = comb_dat_L_WQ, geom="point", ylab = "Average annual in lake chlor-a") + 
+qplot(sumallPC1, chl_a, data = comb_dat_all_WQ, geom="point", ylab = "Average annual chlor-a") + 
   geom_smooth(method = "lm") +
   theme_classic() + theme(axis.text.x = element_text())
 
-chlora.mod_1yr <- glm(chl_a ~ scale(sumallPC1), data = comb_dat_L_WQ)
+#ggsave("PCA_all_ave_chla.pdf",
+#       scale = 2, width = 6, height = 5.25, units = c("cm"), dpi = 300)
+
+chlora.mod_1yr <- glm(chl_a ~ scale(sumallPC1), data = comb_dat_all_WQ)
 summary(chlora.mod_1yr) # year average not significant
 
-# plot chlor-a for in lake averages and pca for extened summer add in linear model 
-qplot(sumallPC1, chl_a, data = comb_dat_WQ, geom="point", ylab = "Average chlor-a ", color = factor(location), shape=factor(depth)) + 
+
+# plot chlor-a for in lake hypo averages and pca for extened summer add in linear model 
+PCA_hypo_ave_chla<- qplot(sumallPC1, chl_a, data = comb_dat_9m_WQ, geom="point", ylab = "Average annual chlor-a (Hypo)", color=depth) + 
+  geom_smooth(method = "lm") +
+  theme_classic() + theme(axis.text.x = element_text())
+PCA_hypo_ave_chla + scale_colour_manual(values = c("darkgreen"))
+
+
+ggsave("PCA_hypo_ave_chla.pdf", scale = 2, width = 6, height = 5.25, units = c("cm"), dpi = 300)
+
+chlora.mod_9myr <- glm(chl_a ~ scale(sumallPC1), data = comb_dat_9m_WQ)
+summary(chlora.mod_9myr) # year average not significant
+
+
+
+# plot chlor-a for in lake (surface, meta, hypo) averages and pca for extened summer add in linear model 
+PCA_allinlake_ave_chla_plot <- qplot(sumallPC1, chl_a, data = comb_dat_inlake_WQ, geom="point", ylab = "Average annual chlor-a (Surface, Meta, Hypo)", color=depth) + 
   geom_smooth(method = "lm") +
   theme_classic() + theme(axis.text.x = element_text())
 
-chlora.mod1 <- lmer(chl_a ~ scale(sumallPC1) + (1 | location), data = comb_dat_WQ)
-summary(chlora.mod1) 
-#inlet outlet might be more responsive 
+PCA_allinlake_ave_chla_plot + scale_colour_manual(values = c("darkolivegreen3", "darkolivegreen4", "darkgreen", "lightseagreen", "cadetblue3"))
 
-# plot chlor-a for in lake and pca for extened summer add in linear model 
-qplot(sumallPC1, chl_a, data = comb_dat_lake_WQ, geom="point", ylab = "Average chlor-a ", color = factor(depth)) + 
+
+ggsave("PCA_allinlake_ave_chla.pdf",
+       scale = 2, width = 6, height = 5.25, units = c("cm"), dpi = 300)
+
+chlora.mod_inlake <- glm(chl_a ~ scale(sumallPC1) + depth, data = comb_dat_inlake_WQ)
+summary(chlora.mod_inlake) # year average not significant
+
+# plot chlor-a for in lake surface averages and pca for extened summer add in linear model 
+qplot(sumallPC1, chl_a, data = comb_dat_0m_WQ, geom="point", ylab = "Average annual chlor-a (Surface)") + 
   geom_smooth(method = "lm") +
   theme_classic() + theme(axis.text.x = element_text())
 
-chlora.mod2 <- glm(chl_a ~ scale(sumallPC1), data = comb_dat_lake_WQ)
-summary(chlora.mod2) # not significant 
+#ggsave("PCA_surface_ave_chla.pdf",
+#       scale = 2, width = 6, height = 5.25, units = c("cm"), dpi = 300)
+
+chlora.mod_0m <- glm(chl_a ~ scale(sumallPC1), data = comb_dat_0m_WQ)
+summary(chlora.mod_0m)
 
 
-# plot chlor-a for inlet and pca for extened summer add in linear model 
-qplot(sumallPC1, chl_a, data = comb_dat_in_WQ, geom="point", ylab = "Average chlor-a inlet", color = factor(depth)) + 
+# plot chlor-a for in lake meta averages and pca for extened summer add in linear model 
+qplot(sumallPC1, chl_a, data = comb_dat_3m_WQ, geom="point", ylab = "Average annual chlor-a (Meta)") + 
   geom_smooth(method = "lm") +
   theme_classic() + theme(axis.text.x = element_text())
 
-chlora.mod2 <- glm(chl_a ~ scale(sumallPC1), data = comb_dat_in_WQ)
-summary(chlora.mod2) # inlet is significant 
+#ggsave("PCA_meta_ave_chla.pdf",
+#       scale = 2, width = 6, height = 5.25, units = c("cm"), dpi = 300)
 
-# plot chlor-a for inlet and pca for extened summer add in linear model 
-qplot(sumallPC1, chl_a, data = , geom="point", ylab = "Average chlor-a inlet", color = factor(depth)) + 
+chlora.mod_3m <- glm(chl_a ~ scale(sumallPC1), data = comb_dat_3m_WQ)
+summary(chlora.mod_3m)
+
+# plot chlor-a for IN and OUT averages and pca for extened summer add in linear model 
+PCA_INOUT_ave_chla <- qplot(sumallPC1, chl_a, data = comb_dat_INOUT_WQ, geom="point", ylab = "Average annual chlor-a (Meta)", color=depth) + 
   geom_smooth(method = "lm") +
   theme_classic() + theme(axis.text.x = element_text())
-
-chlora.mod2 <- glm(chl_a ~ scale(sumallPC1), data = comb_dat_in_WQ)
-summary(chlora.mod2) # inlet is significant 
+PCA_INOUT_ave_chla+scale_colour_manual(values = c("lightseagreen", "cadetblue3"))
 
 
-#comb_dat_in_out_WQ
+ggsave("PCA_INOUT_ave_chla.pdf", scale = 2, width = 6, height = 5.25, units = c("cm"), dpi = 300)
 
-# plot chlor-a for 9m and pca for extened summer add in linear model 
-qplot(sumallPC1, chl_a, data = comb_dat_9m_WQ, geom="point", ylab = "Average chlor-a ") + 
-  geom_smooth(method = "lm") +
+chlora.mod_IN_OUT <- glm(chl_a ~ scale(sumallPC1) + depth, data = comb_dat_INOUT_WQ)
+summary(chlora.mod_IN_OUT)
+
+
+
+
+# plot chlor-a for all averages and pca for extened summer add in linear model 
+ave_chl_plot <- qplot(sumallPC1, chl_a, data = comb_dat_lake_WQ, geom="point", ylab = "Average annual chlor-a (Meta)", color=depth) + 
+  geom_smooth(method = "lm", color=depth) +
   theme_classic() + theme(axis.text.x = element_text())
 
-chlora.mod_9m <- glm(chl_a ~ scale(sumallPC1), data = comb_dat_9m_WQ)
-summary(chlora.mod_9m) # inlet and outlet is more significant 
+ave_chl_plot + scale_colour_manual(values = c("darkolivegreen3", "darkolivegreen4", "darkgreen", "lightseagreen", "cadetblue3"))
 
-#Inlet and outlet chlorophyll-a seem to more sensitive that inlake values
+ggsave("PCA_all_ave_chla.pdf",
+      scale = 2, width = 6, height = 5.25, units = c("cm"), dpi = 300)
 
-## check out chlor-a max 
+chlora.mod_ave_all <- glm(chl_a ~ scale(sumallPC1) + depth, data = comb_dat_lake_WQ)
+summary(chlora.mod_ave_all)
+
+chlora.mod_ave_all <- lmer(chl_a ~ scale(sumallPC1) + (1|depth), data = comb_dat_lake_WQ)
+summary(chlora.mod_ave_all)
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+##### Averages not giving conclusive resluts. 
+#### Look at max values 
+
+## annual average all chlor-a data aty GL4
+WQdat_all_ave <- aggregate(chl_a ~ year, data=WQdat1, FUN=max) 
+WQdat_all_ave$SE <- aggregate(chl_a ~ year, data=WQdat1, FUN=se)[,2]
+
+
+## average chlor-a for all inlake depths by year 
+WQdat_lake_max <- aggregate(chl_a ~ year, data=WQdat_lake, FUN=max) 
+WQdat_lake_max$SE <- aggregate(chl_a ~ year, data=WQdat_lake, FUN=se)[,2]
+
+
+# average annual chla at inlet
+WQdat_IN_max <- aggregate(chl_a ~ year, data= WQdat_in_lake, FUN=max) 
+WQdat_IN_max$SE <- aggregate(chl_a ~ year, data=WQdat_in_lake, FUN=se)[,2]
+
+WQdat_IN_max$depth <- "IN"
+
+# average annual chla at outlet
+WQdat_out_max <- aggregate(chl_a ~ year, data= WQdat_out_lake, FUN=max) 
+WQdat_out_max$SE <- aggregate(chl_a ~ year, data=WQdat_out_lake, FUN=se)[,2]
+
+WQdat_out_max$depth <- "OUT"
+
+# average annual chla at lake surface
+WQdat_0m_max <- aggregate(chl_a ~ year, data= WQdat_lake_0m, FUN=max) 
+WQdat_0m_max$SE <- aggregate(chl_a ~ year, data=WQdat_lake_0m, FUN=se)[,2]
+
+WQdat_0m_max$depth <- "0m"
+
+# average annual chla at metalimnion
+WQdat_3m_max <- aggregate(chl_a ~ year, data= WQdat_lake_3m, FUN=max) 
+WQdat_3m_max$SE <- aggregate(chl_a ~ year, data=WQdat_lake_3m, FUN=se)[,2]
+
+WQdat_3m_max$depth <- "3m"
+
+# average annual chla at hypolimnion 
+WQdat_9m_max<- aggregate(chl_a ~ year, data= WQdat_lake_9m, FUN=max) 
+WQdat_9m_max$SE <- aggregate(chl_a ~ year, data=WQdat_lake_9m, FUN=se)[,2]
+
+WQdat_9m_max$depth <- "9m"
+
+# rbind all aggregated max values
+WQdat_max_alldepth <- rbind(WQdat_IN_max, WQdat_out_max, WQdat_0m_max, WQdat_3m_max, WQdat_9m_max)
+
+## Annual max all chlora at GL4 and  PCA
+comb_dat_all_max_WQ <- left_join(WQdat_max_alldepth, dt_summer[c("eco_year", "sumallPC1")],
+                             by = c("year" = "eco_year"))
+
+
+#########################################
+####### Visulalize Max Chla##############
+
+png("Chlor_A-max_overtime(all locations).png",  
+    width = 5.25,
+    height = 3.25,
+    units = "in",
+    res = 1200,
+    pointsize = 4 )
+
+## Average annual chla for all lake locations
+
+plot(NA, NA, xlim = c(2000, 2017), ylim = c(0, 21), xlab = "Year",
+     ylab = "Average annual chlor-A in GL4", cex.lab=1.5, cex.axis=1.2, 
+     xaxt="n")
+axis(1, at=c(2000:2017), cex.axis=1.5)
+
+lines(y = WQdat_IN_max$chl_a, x= WQdat_IN_max$year, lty =1, lwd = 1, col= "lightseagreen")
+lines(y = WQdat_out_max$chl_a,  x= WQdat_out_max$year, lty =1, lwd = 1, col= "cadetblue3")
+lines(y = WQdat_0m_max$chl_a,  x= WQdat_0m_max$year, lty =1, lwd = 1, col= "darkolivegreen3")
+lines(y = WQdat_3m_max$chl_a,  x= WQdat_3m_max$year, lty =1, lwd = 1, col= "darkolivegreen4")
+lines(y = WQdat_9m_max$chl_a,  x= WQdat_9m_max$year, lty =1, lwd = 1, col= "darkgreen")
+
+
+points(y = WQdat_IN_max$chl_a, x= WQdat_IN_max$year, col= "lightseagreen", pch=19) 
+arrows(WQdat_IN_max$year, (WQdat_IN_max$chl_a + WQdat_IN_max$SE), 
+       WQdat_IN_max$year, (WQdat_IN_max$chl_a - WQdat_IN_max$SE), 
+       length=0.0, code=3, lwd=0.5, col= "lightseagreen")
+
+
+points(y = WQdat_out_max$chl_a, x= WQdat_out_max$year, col= "cadetblue3", pch=19) 
+arrows(WQdat_out_max$year, (WQdat_out_max$chl_a + WQdat_out_max$SE), 
+       WQdat_out_max$year, (WQdat_out_max$chl_a - WQdat_out_max$SE), 
+       length=0.0, code=3, lwd=0.5, col= "cadetblue3")
+
+points(y = WQdat_0m_max$chl_a, x= WQdat_0m_max$year, col= "darkolivegreen3", pch=19) 
+arrows(WQdat_0m_max$year, (WQdat_0m_max$chl_a + WQdat_0m_max$SE), 
+       WQdat_0m_max$year, (WQdat_0m_max$chl_a - WQdat_0m_max$SE), 
+       length=0.0, code=3, lwd=0.5, col= "darkolivegreen3")
+
+points(y = WQdat_3m_max$chl_a, x= WQdat_3m_max$year, col= "darkolivegreen4", pch=19) 
+arrows(WQdat_3m_max$year, (WQdat_3m_max$chl_a + WQdat_3m_max$SE), 
+       WQdat_3m_max$year, (WQdat_3m_max$chl_a - WQdat_3m_max$SE), 
+       length=0.0, code=3, lwd=0.5, col= "darkolivegreen4")
+
+points(y = WQdat_9m_max$chl_a, x= WQdat_9m_max$year, col= "darkgreen", pch=19) 
+arrows(WQdat_9m_max$year, (WQdat_9m_max$chl_a + WQdat_9m_max$SE), 
+       WQdat_9m_max$year, (WQdat_9m_max$chl_a - WQdat_9m_max$SE), 
+       length=0.0, code=3, lwd=0.5, col= "darkgreen")
+
+
+legend("topleft", legend=c("Inlet", "Outlet", "0m", "3m", "9m"),
+       col=c("lightseagreen", "cadetblue3", "darkolivegreen3", "darkolivegreen4", "darkgreen" ), lty=1, cex=1.2)
+
+dev.off()
+
+
+# plot chlor-a for max chlora and pca for extened summer add in linear model 
+pac_max_plot <- qplot(sumallPC1, chl_a, data = comb_dat_all_max_WQ, geom="point", ylab = "Maximum annual chlor-a", color=depth)  +
+  geom_smooth(method = "lm") +
+  theme_classic() + theme(axis.text.x = element_text()) 
+
+pac_max_plot + scale_colour_manual(values = c("darkolivegreen3", "darkolivegreen4", "darkgreen", "lightseagreen", "cadetblue3"))
+
+ggsave("PCA_max_chla.pdf",
+       scale = 2, width = 6, height = 5.25, units = c("cm"), dpi = 300)
+
+chlora.mod_all_max <- glm(chl_a ~ scale(sumallPC1), data = comb_dat_all_max_WQ)
+summary(chlora.mod_all_max)
 
